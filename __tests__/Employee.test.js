e33b4d8363f7f73614821706402fed2f9d3bec91
Steven Nguyen <qviet715@gmail.com>
2023-03-12 12:50:19 -0400

    install jest and employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,18 @@
+const Employee = require ('../lib/Employee');
+
+test ('Does this creat employee object', () => {
+    const emp = new Employee();
+
+    expect(typeof(emp)).toBe('object');
+});
+
+test ('Create an employee name, id, and email', () => {
+    const employee = new Employee("Donnie", 1, "test@test.com");
+
+    expect(employee.name).toBe('Donnie');
+    expect(employee.id).toBe(1);
+    expect(employee.email).toBe('test@test.com');
+    expect(employee.getName()).toBe('Donnie'); 
+
+    expect(employee.getRole()).toBe('Employee');
+})
